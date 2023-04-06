1a537054d47821c0df80a6c64884d11bcbed6c14
Ha2ram17 <127845856+Ha2ram17@users.noreply.github.com>
2023-04-06 18:13:03 +0900

    Update 자바 기초 수업 내용.java

--- 자바 기초 수업 내용.java
+++ 자바 기초 수업 내용.java
@@ -13,7 +13,8 @@ public class Chr {
 		System.out.println("3+5");
 		System.out.print("3+5 = "); //print에 ln을 붙히지 않음으로써 결과론적으로 아랫 문장과 합쳐져서 3+5 = 8이 나옴.
 		System.out.println(3+5);
-		System.out.println("3+5 = "+(3+5));
+		System.out.println("3+5 = "+(3+5)); //(3+5)를 먼저 계산해주므로써, java가 문자열로 인식하지않게끔 해서 3+5 = 8이 나오게 함. 
+		//만약 ("3+5 ="+ 3+5);로 할 시, 3 + 5 = 35가 나옴 
 	}
 
 }
